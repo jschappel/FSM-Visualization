8b8443dccff59b3e00e6652cb51a51cb6b218553
jschappel <jmschappel12@gmail.com>
2019-07-23 16:11:51 -0400

    Finished Adding Buttons
    - All buttons added
    - TODO: Add inputs

--- gui.rkt
+++ gui.rkt
@@ -1,26 +1,37 @@
 #lang racket
 (require 2htdp/image 2htdp/universe "button.rkt")
 
+;; GLOBAL VALIRABLES
 (define WIDTH 1200) ;; The width of the scene
 (define HEIGHT 600) ;; The height of the scene
 (define TOP (/ HEIGHT 10))
 (define RIGHT (/ WIDTH 5))
 (define BOTTOM(/ HEIGHT 8))
+(define CONTROL-BOX-H (/ HEIGHT 5)) ;; The height of each left side conrol box
 (define E-SCENE (empty-scene WIDTH HEIGHT "white")) ;; Create the initial scene
 
-;; posn: integer integer -> posn
-;; Purpose A structure that represents a position
-;; - x: The x coordinate
-;; - y: The y coordinate
-;;(struct posn (x y))
-
-(define CONTROL-BOX-H (/ HEIGHT 5)) ;; The height of each left side conrol box
 ;; **** BUTTONS BELOW ***
-(define BTN-ADD-STATE (button 100 25 "Add" "solid" "red" #f (posn (- WIDTH 100) (- CONTROL-BOX-H 60))))
-(define BTN-REMOVE-STATE (button 100 25 "Remove" "solid" "red" #f (posn (- WIDTH 100) (- CONTROL-BOX-H 25))))
+(define BTN-ADD-STATE (button 70 25 "Add" "solid" (make-color 230 142 174) #f (posn (- WIDTH 150) (- CONTROL-BOX-H 25))))
+(define BTN-REMOVE-STATE (button 70 25 "Remove" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- CONTROL-BOX-H 25))))
 
+(define BTN-ADD-ALPHA (button 70 25 "Add" "solid" (make-color 230 142 174) #f (posn (- WIDTH 150) (- (* 2 CONTROL-BOX-H) 25))))
+(define BTN-REMOVE-ALPHA (button 70 25 "Remove" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- (* 2 CONTROL-BOX-H ) 25))))
 
+(define BTN-ADD-START (button 50 25 "Add" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- (* 3 CONTROL-BOX-H) 71))))
+(define BTN-REMOVE-START (button 50 25 "Remove" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- (* 3 CONTROL-BOX-H) 25))))
 
+(define BTN-ADD-END (button 50 25 "Add" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- (* 4 CONTROL-BOX-H) 71))))
+(define BTN-REMOVE-END (button 50 25 "Remove" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- (* 4 CONTROL-BOX-H) 25))))
+
+(define BTN-ADD-RULES (button 70 25 "Add" "solid" (make-color 230 142 174) #f (posn (- WIDTH 150) (- (* 5 CONTROL-BOX-H) 25))))
+(define BTN-REMOVE-RULES (button 70 25 "Remove" "solid" (make-color 230 142 174) #f (posn (- WIDTH 50) (- (* 5 CONTROL-BOX-H) 25))))
+
+;; BUTTON-LIST: A List containing all buttons that are displayed on the scene.
+(define BUTTON-LIST (list BTN-ADD-STATE BTN-REMOVE-STATE
+                          BTN-ADD-ALPHA BTN-REMOVE-ALPHA
+                          BTN-ADD-START BTN-REMOVE-START
+                          BTN-ADD-END BTN-REMOVE-END
+                          BTN-ADD-RULES BTN-REMOVE-RULES))
 
 
 (define-struct world (string)) ;; TODO add stuff to world
@@ -29,11 +40,18 @@
 
 (define (draw-world w)
   (place-image (create-gui-left) (- WIDTH 100) (/ HEIGHT 2)
-               
                (place-image (create-gui-top) (/ WIDTH 2) (/ TOP 2)
                             (place-image (create-gui-bottom) (/ WIDTH 2) (- HEIGHT (/ BOTTOM 2))
                                          (draw-button BTN-ADD-STATE
-                                                      (draw-button BTN-REMOVE-STATE E-SCENE))))))
+                                                      (draw-button BTN-REMOVE-STATE
+                                                                   (draw-button BTN-ADD-ALPHA
+                                                                                (draw-button BTN-REMOVE-ALPHA
+                                                                                             (draw-button  BTN-ADD-START
+                                                                                                           (draw-button BTN-REMOVE-START
+                                                                                                                        (draw-button BTN-ADD-END
+                                                                                                                                     (draw-button BTN-REMOVE-END
+                                                                                                                                                  (draw-button  BTN-ADD-RULES
+                                                                                                                                                                (draw-button BTN-REMOVE-RULES E-SCENE))))))))))))))
 
 
 ;; top-input-label: null -> image
@@ -94,7 +112,8 @@
    (text (string-upcase "List of rules will go here") 24 "Black")
    (rectangle (- (- WIDTH (/ WIDTH 11)) 200) BOTTOM "outline" "green")))
 
-
+;; create-gui-left: null -> image
+;; Purpose: creates the left conrol panel for the 
 (define (create-gui-left)
   (overlay/align "left" "top"
                  (above/align "left"
@@ -105,44 +124,50 @@
                               (rule-left-control))
                  (rectangle 200 HEIGHT "outline" "gray")))
 
-
+;; state-left-control: null -> image
+;; Purpose: Creates the state control panel
 (define (state-left-control)
   (overlay/align "left" "top"
-                 (rectangle 200 (/ HEIGHT 5) "outline" "blue")
+                 (rectangle 200 CONTROL-BOX-H "outline" "blue")
                  (control-header "State Options")))
-                 
 
+                 
+;; alpha-left-control: null -> image
+;; Purpose: Creates the alpha control panel
 (define (alpha-left-control)
   (overlay/align "left" "top"
-                 (rectangle 200 (/ HEIGHT 5) "outline" "blue")
+                 (rectangle 200 CONTROL-BOX-H "outline" "blue")
                  (control-header "Alpha Options")))
 
 
+;; start-left-control: null -> image
+;; Purpose: Creates the start control panel
 (define (start-left-control)
   (overlay/align "left" "top"
-                 (rectangle 200 (/ HEIGHT 5) "outline" "blue")
+                 (rectangle 200 CONTROL-BOX-H "outline" "blue")
                  (control-header "Start State")))
 
+
+;; end-left-control: null -> image
+;; Purpose: Creates the end control panel
 (define (end-left-control)
   (overlay/align "left" "top"
-                 (rectangle 200 (/ HEIGHT 5) "outline" "blue")
+                 (rectangle 200 CONTROL-BOX-H "outline" "blue")
                  (control-header "End State")))
 
+;; rule-left-control: null -> image
+;; Purpose: Creates the rule control panel
 (define (rule-left-control)
   (overlay/align "left" "top"
-                 (rectangle 200 (/ HEIGHT 5) "outline" "blue")
+                 (rectangle 200 CONTROL-BOX-H "outline" "blue")
                  (control-header "Add Rules")))
   
-
 ;; control-header: string -> image
 ;; Purpose: Creates a header label for right control panel
 (define (control-header msg)
   (overlay
    (text (string-upcase msg) 18 "Black")
-   (rectangle 200 25 "outline" "white")))
-
-
-;;(define (start-left-control))
+   (rectangle 200 25 "outline" "transparent")))
    
 
 
