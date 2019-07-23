980ccbc31a3982bdb8d555535e9581b86f583251
jschappel <jmschappel12@gmail.com>
2019-07-23 15:51:51 -0400

    Updated Button documentation

--- button.rkt
+++ button.rkt
@@ -1,10 +1,11 @@
 #lang racket
-;; draw button
-;; button (struct)
-;; button-pressed?
 (require 2htdp/image 2htdp/universe)
 
-;; Export functions
+;; ------- BUTTON -------
+;; This class holds the buttons functionality and structure
+
+
+;; Export necessary functions
 (provide
  (struct-out button)
  (struct-out posn)
@@ -23,7 +24,6 @@
 ;; - color: String that represents the background color of the button
 ;; - rounded?: Boolean representing the shape of the button. #t if rounded.
 ;; - location: posn that represents the location for the button
-;; - onClick: 
 (struct button (width height text type color rounded? location))
 
 
@@ -33,7 +33,7 @@
   (place-image (create-button btn) (posn-x (button-location btn)) (posn-y (button-location btn)) scn))
 
 ;; create-button: button -> Image
-;; create-button: creates an image that represents the button and then returns it
+;; Purpose: creates an image that represents the button and then returns it
 (define (create-button btn)
   (cond
     [(button-rounded? btn) (overlay (text (string-upcase (button-text btn)) 18 "black")
@@ -43,7 +43,8 @@
               (rectangle (button-width btn) (button-height btn) (button-type btn) (button-color btn)))]))
 
 ;; button-pressed? x y -> boolean
-;; Purpose: When given an x and y corrdinate, will determine if that coordinate was inside the button. If so returns true
+;; Purpose: Determins if a specific button was pressed
+;; When given an x and y corrdinate, will determine if that coordinate was inside the button. If so returns true
 (define (button-pressed? mouse-x mouse-y btn)
   (cond
     [(and (and (> mouse-x (- (posn-x (button-location btn)) (/ (button-width btn) 2)))
